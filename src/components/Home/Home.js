e444daa39a7381b3d486f1c4df50a624fe2b876c
Vanshh29 <vanshchoudhary5c@gmail.com>
2022-03-03 17:28:15 +0100

    text and css edit
    edited the text and css

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -5,19 +5,15 @@ const Home = () => {
     <div className="content">
       <h1>Welcome....</h1>
       <p>We help you to take informed decisions!! </p>
+      <div className='container'>
+        <h1 className='heading'></h1>
+        <p className='text'>We are all in one free vidoegraphy/photgraphy consultancy services.
+Providing specification about cameras and its other equipments.</p>
+      </div>
       <div className='hero'>   <img 
         src="https://images.unsplash.com/photo-1591288685011-819c8f071c36?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
         alt="site banner"
       /></div>
-   
-      <p>
-        Lorem Ipsum is simply dummy text of the printing and typesetting
-        industry. Lorem Ipsum has been the industry's standard dummy text ever
-        since the 1500s, when an unknown printer took a galley of type and
-        scrambled it to make a type specimen book. It has survived not only five
-        centuries, but also the leap into electronic typesetting, remaining
-        essentially unchanged.
-      </p>
     </div>
   );
 };
