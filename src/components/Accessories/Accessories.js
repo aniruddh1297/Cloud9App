1e124743d37d3b8be697d8aead897807e96b7242
Shivani Lohokare <shivanilohokaree@gmail.com>
2022-03-09 02:10:13 +0100

    Basic Skeleton of Accessories page implemented

--- src/components/Accessories/Accessories.js
+++ src/components/Accessories/Accessories.js
@@ -1,13 +1,37 @@
-import './Accessories.css'
+import { NavLink } from 'react-router-dom';
+import './Accessories.css';
+//import SimpleImageSlider from "react-simple-image-slider";
+
 
 const Accessories = () => {
 
     return (
 
         <div className='content'>
-
-            <h1> Where all the accessories will be renedered</h1>
-        </div>
+            <div className="search-container">
+    <form action="/action_page.php">
+      <input type="text" placeholder="Search.." name="search"/>
+      <button type="submit"><i className="fa fa-search"></i></button>
+    </form>
+    </div>
+            
+            <div className='category'> 
+            <p style={{textAlign: "center", fontSize: "x-large"}} >Lens</p><br></br>
+            <p style={{textAlign: "center",  fontSize: "x-large"}}>Tripods</p><br></br>
+            <p style={{textAlign: "center",  fontSize: "x-large"}}>Polarizing Filters</p><br></br>
+            <p style={{textAlign: "center",  fontSize: "x-large"}}>Batteries</p><br></br>
+            <p style={{textAlign: "center",  fontSize: "x-large"}}>Memory Cards</p><br></br>
+            <p style={{textAlign: "center", fontSize: "x-large"}}>Cleaning kits</p><br></br>
+            <p style={{textAlign: "center",  fontSize: "x-large"}}>Camera Straps</p>
+            </div>
+            
+            <div className='infoContainer'>
+            <h3 style={{fontSize:"xx-large", paddingLeft:"80px", paddingtop:"100px"}}> Do You Know?</h3>
+            </div>
+           </div>
+           
+          
+    
     )
 }
 
