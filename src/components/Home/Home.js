32cfb8fb3e42880ab1cf966767dc4ba11f21702a
Vanshh29 <vanshchoudhary5c@gmail.com>
2022-03-11 17:34:26 +0100

    homepage img

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,17 +3,13 @@ import './Home.css';
 const Home = () => {
   return (
     <div className="content">
-      <h1>Welcome....</h1>
-      <p>We help you to take informed decisions!! </p>
-      <div className='container'>
-        <h1 className='heading'>About us</h1>
-        <p className='text'>We are all in one free vidoegraphy/photgraphy consultancy services.
-Providing specification about cameras and its other equipments.</p>
-      </div>
-      <div className='hero'>  <img 
-        src="https://images.unsplash.com/photo-1591288685011-819c8f071c36?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
-        alt="site banner"
-      /></div>
+      <div className='home' >Home</div>
+      <div className='hotcams'>HOT CAMS</div>
+      <div className='accessories'>ACCESSORIES</div>
+      <div className='software'>SOFTWARE</div>
+      <div className='forum'>FORUM</div>
+      <div className='aboutus'>ABOUT US</div>
+      <p className='tagline'>Capture Moments Make Memories</p>
     </div>
   );
 };
